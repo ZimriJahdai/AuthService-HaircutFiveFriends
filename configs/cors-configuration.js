6844e4ec872968264314feaebb80520ea1d4015a
Marcos <mgarcia-2024412@kinal.edu.gt>
2026-07-29 02:58:58 -0600

    c12

--- configs/cors-configuration.js
+++ configs/cors-configuration.js
@@ -1,5 +1,20 @@
+const allowedOrigins = [
+  'https://movil-haircutfivefriends-production.up.railway.app',
+  'https://authservice-haircutfivefriends-production.up.railway.app',
+  'https://frontend-haircutfivefriends-production.up.railway.app',
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://localhost:8081',
+];
+
 export const corsOptions = {
-  origin: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
